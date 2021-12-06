8fb7b19f668b4297b63728ac12a7a5f04f8cfeef
Simon Stanlake <simon.stanlake@gmail.com>
2021-12-06 10:07:30 -0800

    rename key to clarify it is a slack app token

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   socketMode:true, // enable the following to use socket mode
-  appToken: process.env.APP_TOKEN
+  appToken: process.env.SLACK_APP_TOKEN
 });
 
 // usage instructions
